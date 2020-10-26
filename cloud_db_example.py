226725d9cd3160ca0db111591d85886da7011ffc
TONYYU\Tony Yu <tonyatpeking@gmail.com>
2020-10-25 21:12:11 -0500

    added examples for local and cloud db

---
+++ cloud_db_example.py
@@ -0,0 +1,24 @@
+import pymongo
+from config import *
+
+ServerSelectionTimeoutError = pymongo.errors.ServerSelectionTimeoutError
+
+# connect to the db using test_user
+# test_user only has read access
+try:
+    client = pymongo.MongoClient(
+        TEST_DB_URI,
+        serverSelectionTimeoutMS=3000
+    )
+
+    mydb = client[DB_NAME]
+    drivers_col = mydb[COL_DRIVERS]
+    print(drivers_col.find_one())
+
+    routes_col = mydb[COL_ROUTES]
+    print(routes_col.find_one())
+
+
+except ServerSelectionTimeoutError as e:
+    raise ServerSelectionTimeoutError(
+        'Error connecting, make sure your IP is whitelisted, changes may take some time to take effect') from e
---
+++ local_db_example.py
@@ -0,0 +1,12 @@
+import pymongo
+from config import *
+
+myclient = pymongo.MongoClient(LOCAL_URI)
+mydb = myclient["mydatabase"]
+mycol = mydb["customers"]
+
+mydict = {"name": "John", "address": "Highway 37"}
+
+x = mycol.insert_one(mydict)
+
+print(mycol.find_one())
